654787c14cbe46684478db4cc0787455f7b3d7d8
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-06-28 12:10:54 -0700

    zoo method

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,28 @@
 class Zoo
+  attr_reader :location
+  attr_accessor :name
+  @@all = []
+  def initialize(name, location)
+    @name = name
+    @location = location
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def animals
+    Animal.all.select do |animal|
+      animal.zoo == self
+    end
+  end
+
+
+  def animal_species
+    animals.map do |animal|
+      animal.species
+  end
+
+  
 end
